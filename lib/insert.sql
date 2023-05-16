20dfc1d7c75e049d894727de87ae9566cfc21170
Tina Shi <init.defaultBranch>
2023-05-16 20:14:40 +1000

    commit tree 22043eac51a19f7b1e1db6f98414ca6b9ea394fc

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO
+bears(name, sex)
+VALUES
+("Mr. Chocolate", "M"),
+("Rowdy", "M"),
+("Tabitha", "M"),
+("Sergeant Brown", "M"),
+("Melissa", "F"),
+("Grinch", "M"),
+("Wendy", "F"),
+(null, "F")
